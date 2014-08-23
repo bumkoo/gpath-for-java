38ca23041901101f95425259dec139f41dd20a91
Bekay <bumkoo.choi@gmail.com>
2014-08-23 12:13:34 +0900

    init

---
+++ src/test/groovy/Build.groovy
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+class Build {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	
+	
+//https://github.com/bumkoo/gpath-for-java.git 3
+}
